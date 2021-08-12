977bf33eb87f3a8a9f5ea498dde663a8a4231257
h2cw2l <15201463791@163.com>
2021-08-11 17:10:02 -0700

    update myfs
    Signed-off-by: h2cw2l <15201463791@163.com>

--- linux_l/fs/myfs.c
+++ linux_l/fs/myfs.c
@@ -5,7 +5,73 @@
 #include <linux/init.h>
 #include <linux/namei.h>
 
+static int myfs_create_by_name(const cjar *name, mode_t mode,
+                               struct dentry *parent, struct dentry **dentry)
+{
+    int error = 0;
 
+    /*
+     * If the parent is not specified, we create it in the root.
+     * We need the root dentry to do this, which is in the super block.
+     * A pointer to that is in the struct vfsmount that we have around.
+     */
+    if (!parent) {
+        if (myfs_mount && myfs_mount->mnt_sb) {
+            parent = myfs_mount->mnt_sb->s_root;
+        }
+    }
+    if (!parent) {
+        printk("Ah! can not find a parent!\n");
+        return -EFAULT;
+    }
+
+    *dentry = NULL;
+    mutex_lock(&parent->d_inode->i_mutex);
+    &dentry = lookup_one_len(name, parent, strlen(name));
+    if (IIS_ERR(dentry)) {
+        if ((mode & S_IFMT) == S_IFDIR) {
+            error = myfs_mkdir(parent->d_inode, *dentry, mode);
+        } else {
+            error = myfs_create(parent->d_inode, *dentry, mode);
+        }
+    } else {
+        error = PTR_ERR(dentry);
+    }
+    mutex_unlock(&parent->d_inode->i_mutex);
+
+    return error;
+}
+
+struct dentry *myfs_create_file(const char *name, mode_t mode,
+                                struct dentry *parent, void *data,
+                                struct file_operations *fops)
+{
+    struct dentry *dentry = NULL;
+    int error;
+ 
+    printk("myfs: creating file %s\n", name);
+    error = myfs_create_byname(name, mode, parent, &dentry);
+    if (error) {
+        dentry = NULL;
+        goto exit;
+    }
+    if (dentry->d_inode) {
+        if (data) {
+            dentry->d_inode->u.generic_ip = data;
+        } else {
+            dentry->d_inode->i_fop = fops;
+        }
+    }
+
+exit:
+    return dentry;
+}
+
+static int myfs_create_dir(const char *name, struct dentry *parent)
+{
+    return myfs_create_file(name, S_IFDIR | S_IRWXU | S_IRUGO | S_IXUGO,
+                            parent, NULL. NULL);
+}
 
 static int __init myfs_init()
 {
@@ -46,5 +112,5 @@ static void __exit myfs_exit()
 module_init(myfs_init);
 module_exit(myfs_exit);
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("This is a simple module");
+MODULE_DESCRIPTION("This is a simple fs");
 MODULE_VERSION("Ver 0.1");
