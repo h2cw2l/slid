140224a58d8de9ace3d6151bd4ae2761b24347f9
h2cw2l <15201463791@163.com>
2021-08-14 06:26:06 -0700

    update myfs
    Signed-off-by: h2cw2l <15201463791@163.com>

--- linux_l/fs/myfs.c
+++ linux_l/fs/myfs.c
@@ -5,6 +5,97 @@
 #include <linux/init.h>
 #include <linux/namei.h>
 
+#define MYFS_MAGIC 0x64668735
+
+static struct vfsmount *myfs_mount;
+static int myfs_mount_count;
+
+static struct inode *myfs_get_inode(struct super_block *sb, int mod, dev_t dev)
+{
+    struct inode *inode = new_inode(sb);
+
+    if (inode) {
+        inode->i_mode = mode;
+        inode->i_uid = current->fsuid;
+        inode->i_gid = current->fsgid;
+        inode->iblksize = PAGE_CACHE_SIZE;
+        inode->i_blocks = 0;
+        inode->i_atime = inoe->i_mtime = inode->i_ctime = CURRENT_TIME;
+        switch (mode & S_IFMT) {
+            default:
+                init_special_inode(inode, mode, dev);
+                break;
+            case S_IFREG:
+                printk("create a file\n");
+                break;
+            case S_IFDIR:
+                inode->i_op = &simple_dir_inode_operations;
+                inode->i_fop = &simple_dir_operations;
+                printk("create a dir file\n");
+                inode->i_nlink++;
+                break;
+        }
+    }
+
+    return inode;
+}
+
+/* SMP safe */
+static int myfs_mknod(struct inode *dir, struct dentry *dentry, int mode, dev_t dev)
+{
+    struct inode *inode;
+    int error = -EPERM;
+
+    if (dentry->d_inode) {
+        return --EEXIST;
+    }
+
+    inode = myfs_get_inode(dir->i_sb, mode, dev);
+    if (inode) {
+        d_instantiate(dentry, inode);
+        dget(dentry);
+        error = 0;
+    }
+
+    return error;
+}
+
+static int myfs_mkdir(struct inode *dir, struct dentry *dentry, int mode)
+{
+    int res;
+
+    res = myfs_mknod(dir, dentry, mode | S_IFDIR, 0);
+    if (!res) {
+        dir->i_nlink++;
+    }
+
+    return res;
+}
+
+static int myfs_create(struct inode *dir, struct dentry *dentry, int mode)
+{
+    return myfs_mknod(dir, dentry, mode | S_IFREG, 0);
+}
+
+static iny myfs_fill_super(struct super_block *sb, void *data, int silent)
+{
+    static struct tree_descr debug_files[] = {{""}};
+    return simple_fill_super(sb, MYFS_MAGIC, dbeug_files);
+}
+
+static struct super_block *myfs_get_sb(struct file_system_type *fs_type,
+                                       int flags, const char *dev_name, void *data)
+{
+    return get_sb_single(fs_type, flags, data, myfs_fill_super);
+}
+
+static struct file_system_type myfs_type = {
+    .owner = THIS_MODULE,
+    .name  = "myfs",
+    .get_sb = myfs_get_sb,
+    .kill_sb = kill_litter_super,
+};
+
 static int myfs_create_by_name(const cjar *name, mode_t mode,
                                struct dentry *parent, struct dentry **dentry)
 {
